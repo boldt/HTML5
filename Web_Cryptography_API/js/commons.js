ad33304a247ed29c6ea73d415a2ca3727899a43d
Dennis Boldt <boldt@itm.uni-luebeck.de>
2014-10-17 16:51:49 +0200

    Disable HTTPS on localhost

--- Web_Cryptography_API/js/commons.js
+++ Web_Cryptography_API/js/commons.js
@@ -1,7 +1,17 @@
 // http://stackoverflow.com/a/4723302/605890
-if (window.location.protocol != "https:") {
+
+if (window.location.hostname != "localhost" && window.location.protocol != "https:") {
 	window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
 }
+
+var ArrayBufferToSimpleArray = function(ab) {
+    var arr = [];
+    var uint8 = new Uint8Array(ab);
+    for(var i = 0; i<uint8.length; i++) {
+        arr.push(uint8[i]);
+    }
+    return arr;
+};
 
 var sha1ArrayBufferToString = function(buffer) {
 
