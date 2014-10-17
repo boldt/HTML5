e5cf55451e59072976de18dd388eb6eccc7a8fbd
Dennis Boldt <boldt@itm.uni-luebeck.de>
2014-10-17 17:28:06 +0200

    RSA-OAEP + AES-CBC combination works

--- Web_Cryptography_API/js/aes-cbc.js
+++ Web_Cryptography_API/js/aes-cbc.js
@@ -9,25 +9,23 @@ var aes_cbc = function (buf) {
      * 
 	 */
 
-	var algorithmName = 'AES-CBC';
-	var extractable = true;
-	var keyUsages = ['encrypt', 'decrypt'];
-	var keyLengthBits = 128;
-
-	var aesKeyGenParams = { name: algorithmName, length: keyLengthBits };
-	var results = {};
-
-	var ivBuf = crypto.getRandomValues(new Uint8Array(16))
-	var aesCbcParams  = { name: "AES-CBC", iv: ivBuf };
+    var aes = {};
+	aes.algorithmName = 'AES-CBC';
+	aes.extractable = true;
+	aes.keyUsages = ['encrypt', 'decrypt'];
+	aes.keyLengthBits = 128;
+	aes.aesKeyGenParams = { name: aes.algorithmName, length: aes.keyLengthBits };
+	aes.ivBuf = crypto.getRandomValues(new Uint8Array(16))
+	aes.aesCbcParams  = { name: "AES-CBC", iv: aes.ivBuf };
 
 	console.log("Raw data", buf)
 
 	// Generate random key
-	crypto.subtle.generateKey(aesKeyGenParams, extractable, keyUsages).then(function(key_gen) {
+	crypto.subtle.generateKey(aes.aesKeyGenParams, aes.extractable, aes.keyUsages).then(function(key_gen) {
 		console.log("Generated key", key_gen);
 
 		// AES enc
-		crypto.subtle.encrypt(aesCbcParams , key_gen, buf).then(function(data_enc) {
+		crypto.subtle.encrypt(aes.aesCbcParams , key_gen, buf).then(function(data_enc) {
 			console.log("Encrypted data: ", new Uint8Array(data_enc));
 
 			// Key --> AB
@@ -35,11 +33,11 @@ var aes_cbc = function (buf) {
 				console.log("Exported key", new Uint8Array(key_ab));
 
 				// AB --> Key
-				crypto.subtle.importKey("raw", key_ab, { name: algorithmName }, true, keyUsages).then(function(key_imp) {
+				crypto.subtle.importKey("raw", key_ab, { name: aes.algorithmName }, true, aes.keyUsages).then(function(key_imp) {
 					console.log("Imported key", key_imp);
 
 					// AES dec
-					crypto.subtle.decrypt(aesCbcParams , key_imp, data_enc).then(function(data_dec) {
+					crypto.subtle.decrypt(aes.aesCbcParams , key_imp, data_enc).then(function(data_dec) {
 						console.log("Decrypted data: ", new Uint8Array(data_dec));		
 					});
 				});
--- js/commons.js
+++ js/commons.js
@@ -1,10 +1,9 @@
 // http://stackoverflow.com/a/4723302/605890
-
 if (window.location.hostname != "localhost" && window.location.protocol != "https:") {
 	window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
 }
 
-var ArrayBufferToSimpleArray = function(ab) {
+var arrayBufferToSimpleArray = function(ab) {
     var arr = [];
     var uint8 = new Uint8Array(ab);
     for(var i = 0; i<uint8.length; i++) {
